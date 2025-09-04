3f51b84c804892facd4e6202fe7710851b2545b5
ASHUTOSH-SWAIN-GIT <ashutoshswain7383@gmail.com>
2025-09-04 23:12:53 +0530

    handler test

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -84,7 +84,7 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
 	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
 	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
-	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
+	form.CheckField(validator.PermittedValue(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
 	if !form.Valid() {
 		data := app.newTemplateData(r)
 		data.Form = form
@@ -200,16 +200,19 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
 	err := app.sessionManager.RenewToken(r.Context())
-	
-	if err !=  nil {
-		app.serverError(w , err)
+
+	if err != nil {
+		app.serverError(w, err)
 	}
 
 	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
 
-	app.sessionManager.Put(r.Context(), "flash" , "You have been logout succesfully ")
+	app.sessionManager.Put(r.Context(), "flash", "You have been logout succesfully ")
 
-	http.Redirect(w , r , "/" , http.StatusSeeOther)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 
+}
 
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
 }
--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -17,7 +17,7 @@ func Matches(value string, rx *regexp.Regexp) bool {
 }
 
 type Validator struct {
-	FieldErrors map[string]string
+	FieldErrors    map[string]string
 	NonFieldErrors []string
 }
 
@@ -25,8 +25,8 @@ func (v *Validator) Valid() bool {
 	return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) == 0
 }
 
-func (v *Validator) AddNonFieldError(message string){
-	v.NonFieldErrors = append(v.NonFieldErrors , message)
+func (v *Validator) AddNonFieldError(message string) {
+	v.NonFieldErrors = append(v.NonFieldErrors, message)
 }
 
 func (v *Validator) AddFieldError(key, message string) {
@@ -54,6 +54,15 @@ func MaxChars(value string, n int) bool {
 }
 
 func PermittedInt(value int, permittedValues ...int) bool {
+	for i := range permittedValues {
+		if value == permittedValues[i] {
+			return true
+		}
+	}
+	return false
+}
+
+func PermittedValue[T comparable](value T, permittedValues ...T) bool {
 	for i := range permittedValues {
 		if value == permittedValues[i] {
 			return true
