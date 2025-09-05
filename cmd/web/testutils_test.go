87b31869bc2eeeaaf0cf08a7e85d380a371192f1
ASHUTOSH-SWAIN-GIT <ashutoshswain7383@gmail.com>
2025-09-05 11:23:32 +0530

    signup user test

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"bytes"
+	"html"
 	"io"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"net/url"
+	"regexp"
 	"testing"
 	"time"
 
@@ -16,17 +19,13 @@ import (
 )
 
 func newTestApplication(t *testing.T) *application {
-	// Create an instance of the template cache.
 	templateCache, err := newTemplateCache()
 	if err != nil {
 		t.Fatal(err)
 	}
-	// And a form decoder.
+
 	formDecoder := form.NewDecoder()
-	// And a session manager instance. Note that we use the same settings as
-	// production, except that we *don't* set a Store for the session manager.
-	// If no store is set, the SCS package will default to using a transient
-	// in-memory store, which is ideal for testing purposes.
+
 	sessionManager := scs.New()
 	sessionManager.Lifetime = 12 * time.Hour
 	sessionManager.Cookie.Secure = true
@@ -73,5 +72,33 @@ func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, strin
 		t.Fatal(err)
 	}
 	bytes.TrimSpace(body)
+	return rs.StatusCode, rs.Header, string(body)
+}
+
+var csrfTokenRX = regexp.MustCompile(`<input type='hidden' name='csrf_token' value='(.+)'>`)
+
+func extractCSRFToken(t *testing.T, body string) string {
+
+	matches := csrfTokenRX.FindStringSubmatch(body)
+	if len(matches) < 2 {
+		t.Fatal("no csrf token found in body")
+	}
+	return html.UnescapeString(string(matches[1]))
+}
+
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, string) {
+	rs, err := ts.Client().PostForm(ts.URL+urlPath, form)
+	if err != nil {
+		t.Fatal(err)
+	}
+	
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	bytes.TrimSpace(body)
+
+	
 	return rs.StatusCode, rs.Header, string(body)
 }
--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -1,13 +1,22 @@
-package assert 
+package assert
 
 import (
+	"strings"
 	"testing"
 )
 
-func Equal [T comparable] (t *testing.T , actual , expected T){
+func Equal[T comparable](t *testing.T, actual, expected T) {
 	t.Helper()
 
-	if actual !=  expected {
-		t.Errorf("got: %v ; want: %v " , actual ,expected)
+	if actual != expected {
+		t.Errorf("got: %v ; want: %v ", actual, expected)
+	}
+}
+
+func StringContains(t *testing.T, actual, expectedSubstring string) {
+	t.Helper()
+
+	if !strings.Contains(actual, expectedSubstring) {
+		t.Errorf("got: %q; expected to contain: %q", actual, expectedSubstring)
 	}
 }
